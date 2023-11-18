c881fd4a7fea790a723ae4c2a4436ca1fd7f68ac
iuliia fedotchenko <fedotchenko.iuliia@gmail.com>
2023-11-18 14:16:27 +0000

    new commit

--- app/main.py
+++ app/main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "We Love Datascientest 3, and we did it. We build a CI/CD Pipeline !!"}
+    return {"Hello": "111We Love Datascientest 3, and we did it. We build a CI/CD Pipeline !!"}
